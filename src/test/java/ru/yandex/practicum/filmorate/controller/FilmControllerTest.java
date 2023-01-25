a76e9ec5c1097492c986ce87adae33b621bb2384
kmmins <kmmins@yandex.ru>
2023-01-25 14:37:40 +0300

    fix: исправления в классах FilmControllerTest и UserControllerTest.

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -54,85 +54,86 @@ public class FilmControllerTest {
                 () -> filmController.addFilm(film4));
         final ValidationException e4 = assertThrows(ValidationException.class, () -> filmController.addFilm(film5));
 
-        assertEquals("Название фильма не может быть пустым.", e1.getMessage());
-        assertEquals("Максимальная длина описания фильма — 200 символов.", e2.getMessage());
+        assertEquals("addFilm.film.name: не должно быть пустым", e1.getMessage());
+        assertEquals("addFilm.film.description: размер должен находиться в диапазоне от 0 до 200", e2.getMessage());
         assertEquals("Дата релиза фильма не может быть раньше 28 декабря 1895 года.", e3.getMessage());
-        assertEquals("Продолжительность фильма должна быть положительной.", e4.getMessage());
+        assertEquals("addFilm.film.duration: должно быть больше 0", e4.getMessage());
     }
 
     @Test
     void checkUpdFilm() {
-        var film1 = new Film();
-        film1.setName("фильм1");
-        film1.setDescription("описание1");
-        film1.setReleaseDate(LocalDate.of(2023, 1, 1));
-        film1.setDuration(91);
-        var addedFilm1 = filmController.addFilm(film1);
-        var film0 = new Film(100500);
-        film0.setName("фильм0");
-        film0.setDescription("описание0");
-        film0.setReleaseDate(LocalDate.of(2023, 1, 1));
-        film0.setDuration(91);
-        var film1upd1 = new Film(addedFilm1.getId());
-        film1upd1.setName("");
-        film1upd1.setDescription("описание1 обновлено1");
-        film1upd1.setReleaseDate(LocalDate.of(2022, 1, 1));
-        film1upd1.setDuration(89);
-        var film1upd2 = new Film(addedFilm1.getId());
-        film1upd2.setName("фильм1 обновлен2");
-        film1upd2.setDescription("описание1 обновлено2---------------------------------------------------------------" +
-                "---------------------------------------------------------------------------------------------------" +
-                "больше 200 символов");
-        film1upd2.setReleaseDate(LocalDate.of(2022, 1, 1));
-        film1upd2.setDuration(89);
-        var film1upd3 = new Film(addedFilm1.getId());
-        film1upd3.setName("фильм1 обновлен3");
-        film1upd3.setDescription("описание1 обновлено3");
-        film1upd3.setReleaseDate(LocalDate.of(1895, 12, 27));
-        film1upd3.setDuration(89);
-        var film1upd4 = new Film(addedFilm1.getId());
-        film1upd4.setName("фильм1 обновлен4");
-        film1upd4.setDescription("описание1 обновлено4");
-        film1upd4.setReleaseDate(LocalDate.of(2022, 1, 1));
-        film1upd4.setDuration(-89);
-
-        final FilmNotFoundException e0 = assertThrows(FilmNotFoundException.class, () -> filmController.updFilm(film0));
-        final ValidationException e1 = assertThrows(ValidationException.class, () -> filmController.updFilm(film1upd1));
-        final ValidationException e2 = assertThrows(ValidationException.class, () -> filmController.updFilm(film1upd2));
-        final CustomValidationException e3 = assertThrows(CustomValidationException.class,
-                () -> filmController.updFilm(film1upd3));
-        final ValidationException e4 = assertThrows(ValidationException.class, () -> filmController.updFilm(film1upd4));
-
-        assertEquals("Не возможно обновить фильм. Такого фильма не существует.", e0.getMessage());
-        assertEquals("Название фильма не может быть пустым.", e1.getMessage());
-        assertEquals("Максимальная длина описания фильма — 200 символов.", e2.getMessage());
-        assertEquals("Дата релиза фильма не может быть раньше 28 декабря 1895 года.", e3.getMessage());
-        assertEquals("Продолжительность фильма должна быть положительной.", e4.getMessage());
-    }
-
-    @Test
-    void checkGetFilms() {
-        var getAll = filmController.getFilms();
-        int sizeBefore = getAll.size();
-        var film1 = new Film();
-        film1.setName("фильм1");
-        film1.setDescription("описание1");
-        film1.setReleaseDate(LocalDate.of(2023, 1, 1));
-        film1.setDuration(91);
-        filmController.addFilm(film1);
         var film2 = new Film();
         film2.setName("фильм2");
         film2.setDescription("описание2");
         film2.setReleaseDate(LocalDate.of(2023, 1, 2));
         film2.setDuration(92);
-        filmController.addFilm(film2);
+        var addedFilm2 = filmController.addFilm(film2);
+        var film0 = new Film(100500);
+        film0.setName("фильм0");
+        film0.setDescription("описание0");
+        film0.setReleaseDate(LocalDate.of(2023, 1, 2));
+        film0.setDuration(92);
+        var film2upd1 = new Film(addedFilm2.getId());
+        film2upd1.setName("");
+        film2upd1.setDescription("описание2 обновлено1");
+        film2upd1.setReleaseDate(LocalDate.of(2023, 1, 2));
+        film2upd1.setDuration(92);
+        var film2upd2 = new Film(addedFilm2.getId());
+        film2upd2.setName("фильм2 обновлен2");
+        film2upd2.setDescription("описание2 обновлено2---------------------------------------------------------------" +
+                "---------------------------------------------------------------------------------------------------" +
+                "больше 200 символов");
+        film2upd2.setReleaseDate(LocalDate.of(2023, 1, 2));
+        film2upd2.setDuration(92);
+        var film2upd3 = new Film(addedFilm2.getId());
+        film2upd3.setName("фильм1 обновлен3");
+        film2upd3.setDescription("описание1 обновлено3");
+        film2upd3.setReleaseDate(LocalDate.of(1895, 12, 27));
+        film2upd3.setDuration(92);
+        var film2upd4 = new Film(addedFilm2.getId());
+        film2upd4.setName("фильм1 обновлен4");
+        film2upd4.setDescription("описание1 обновлено4");
+        film2upd4.setReleaseDate(LocalDate.of(2022, 1, 1));
+        film2upd4.setDuration(-92);
 
-        getAll = filmController.getFilms();
+        final FilmNotFoundException e0 = assertThrows(FilmNotFoundException.class, () -> filmController.updFilm(film0));
+        final ValidationException e1 = assertThrows(ValidationException.class, () -> filmController.updFilm(film2upd1));
+        final ValidationException e2 = assertThrows(ValidationException.class, () -> filmController.updFilm(film2upd2));
+        final CustomValidationException e3 = assertThrows(CustomValidationException.class,
+                () -> filmController.updFilm(film2upd3));
+        final ValidationException e4 = assertThrows(ValidationException.class, () -> filmController.updFilm(film2upd4));
 
-        assertNotNull(getAll, "Метод вернул null");
-        assertFalse(getAll.isEmpty(), "Список фильмов пуст");
-        assertEquals(sizeBefore + 2, getAll.size(), "Неверное количество фильмов.");
-        assertEquals(film1, getAll.get(sizeBefore), "Фильмы не совпадают.");
-        assertEquals(film2, getAll.get(sizeBefore + 1), "Фильмы не совпадают.");
+        assertEquals("Не возможно обновить фильм. Такого фильма не существует.", e0.getMessage());
+        assertEquals("updFilm.film.name: не должно быть пустым", e1.getMessage());
+        assertEquals("updFilm.film.description: размер должен находиться в диапазоне от 0 до 200", e2.getMessage());
+        assertEquals("Дата релиза фильма не может быть раньше 28 декабря 1895 года.", e3.getMessage());
+        assertEquals("updFilm.film.duration: должно быть больше 0", e4.getMessage());
+    }
+
+    @Test
+    void checkGetFilms() {
+        var getAllBefore = filmController.getFilms();
+        int sizeBefore = getAllBefore.size();
+        var film3 = new Film();
+        film3.setName("фильм3");
+        film3.setDescription("описание3");
+        film3.setReleaseDate(LocalDate.of(2023, 1, 3));
+        film3.setDuration(93);
+        var addedFilm3 = filmController.addFilm(film3);
+        var film4 = new Film();
+        film4.setName("фильм4");
+        film4.setDescription("описание4");
+        film4.setReleaseDate(LocalDate.of(2023, 1, 4));
+        film4.setDuration(94);
+        var addedFilm4 = filmController.addFilm(film4);
+
+        var getAllAfter = filmController.getFilms();
+        int sizeAfter = getAllAfter.size();
+
+        assertNotNull(getAllAfter, "Метод вернул null");
+        assertFalse(getAllAfter.isEmpty(), "Список фильмов пуст");
+        assertEquals(sizeBefore + 2, sizeAfter, "Неверное количество фильмов.");
+        assertEquals(addedFilm3, getAllAfter.get(sizeBefore), "Фильмы не совпадают.");
+        assertEquals(addedFilm4, getAllAfter.get(sizeBefore + 1), "Фильмы не совпадают.");
     }
 }
--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -53,76 +53,79 @@ public class UserControllerTest {
         var getAll = userController.getUsers();
         var checkUser5 = getAll.get(1);
 
-        assertEquals("Электронная почта не может быть пустой и должна содержать символ — @.", e1.getMessage());
+        assertEquals("addUser.user.email: должно иметь формат адреса электронной почты", e1.getMessage());
         assertEquals("addUser.user.login: не должно быть пустым", e2.getMessage());
-        assertEquals("Дата рождения не может быть в будущем.", e3.getMessage());
+        assertEquals("addUser.user.birthday: должно содержать прошедшую дату", e3.getMessage());
         assertEquals("login5", checkUser5.getName(), "Некорректное имя пользователя.");
     }
 
     @Test
     void checkUpdUser() {
-        var user1 = new User();
-        user1.setEmail("user1@user.com");
-        user1.setLogin("login1");
-        user1.setName("username1");
-        user1.setBirthday(LocalDate.of(1989, 4, 20));
-        var addedUser = userController.addUser(user1);
-        var user0 = new User(100500);
-        user0.setEmail("user0@user.com");
-        user0.setLogin("login0");
-        user0.setName("username0");
-        user0.setBirthday(LocalDate.of(1989, 4, 20));
-        var user1upd1 = new User(addedUser.getId());
-        user1upd1.setEmail("");
-        user1upd1.setLogin("login1Upd1");
-        user1upd1.setName("username1Upd1");
-        user1upd1.setBirthday(LocalDate.of(1988, 4, 20));
-        var user1upd2 = new User(addedUser.getId());
-        user1upd2.setEmail("user1@userUpd2.com");
-        user1upd2.setLogin("  ");
-        user1upd2.setName("username1Upd2");
-        user1upd2.setBirthday(LocalDate.of(1988, 4, 20));
-        var user1upd3 = new User(addedUser.getId());
-        user1upd3.setEmail("user1@userUpd3.com");
-        user1upd3.setLogin("login1Upd3");
-        user1upd3.setName("username1Upd3");
-        user1upd3.setBirthday(LocalDate.of(2024, 4, 20));
-
-        final UserNotFoundException e0 = assertThrows(UserNotFoundException.class, () -> userController.updUser(user0));
-        final ValidationException e1 = assertThrows(ValidationException.class, () -> userController.updUser(user1upd1));
-        final ValidationException e2 = assertThrows(ValidationException.class, () -> userController.updUser(user1upd2));
-        final ValidationException e3 = assertThrows(ValidationException.class, () -> userController.updUser(user1upd3));
-
-        assertEquals("Не возможно обновить данные пользователя. Такого пользователя с таким id не существует.",
-                e0.getMessage());
-        assertEquals("Электронная почта не может быть пустой и должна содержать символ — @.", e1.getMessage());
-        assertEquals("Логин не может быть пустым и содержать пробелы.", e2.getMessage());
-        assertEquals("Дата рождения не может быть в будущем.", e3.getMessage());
-    }
-
-    @Test
-    void checkGetUsers() {
-        var getAll = userController.getUsers();
-        var sizeBefore = getAll.size();
-        var user1 = new User(1);
-        user1.setEmail("user1@user.com");
-        user1.setLogin("login1");
-        user1.setName("username1");
-        user1.setBirthday(LocalDate.of(1989, 4, 20));
-        userController.addUser(user1);
-        var user2 = new User(2);
+        var user2 = new User();
         user2.setEmail("user2@user.com");
         user2.setLogin("login2");
         user2.setName("username2");
         user2.setBirthday(LocalDate.of(1989, 4, 21));
-        userController.addUser(user2);
+        var addedUser2 = userController.addUser(user2);
 
-        getAll = userController.getUsers();
+        var user0 = new User(100500);
+        user0.setEmail("user0@user.com");
+        user0.setLogin("login0");
+        user0.setName("username0");
+        user0.setBirthday(LocalDate.of(1989, 4, 21));
 
-        assertNotNull(getAll, "Метод вернул null");
-        assertFalse(getAll.isEmpty(), "Список фильмов пуст");
-        assertEquals(sizeBefore + 2, getAll.size(), "Неверное количество фильмов.");
-        assertEquals(user1, getAll.get(sizeBefore), "Фильмы не совпадают.");
-        assertEquals(user2, getAll.get(sizeBefore + 1), "Фильмы не совпадают.");
+        var user2upd1 = new User(addedUser2.getId());
+        user2upd1.setEmail(" ");
+        user2upd1.setLogin("login2Upd1");
+        user2upd1.setName("username2Upd1");
+        user2upd1.setBirthday(LocalDate.of(1988, 4, 21));
+        var user2upd2 = new User(addedUser2.getId());
+        user2upd2.setEmail("user2@userUpd2.com");
+        user2upd2.setLogin("  ");
+        user2upd2.setName("username2Upd2");
+        user2upd2.setBirthday(LocalDate.of(1988, 4, 21));
+        var user2upd3 = new User(addedUser2.getId());
+        user2upd3.setEmail("user2@userUpd3.com");
+        user2upd3.setLogin("login2Upd3");
+        user2upd3.setName("username2Upd3");
+        user2upd3.setBirthday(LocalDate.of(2024, 4, 21));
+
+        final UserNotFoundException e0 = assertThrows(UserNotFoundException.class, () -> userController.updUser(user0));
+        final ValidationException e1 = assertThrows(ValidationException.class, () -> userController.updUser(user2upd1));
+        final ValidationException e2 = assertThrows(ValidationException.class, () -> userController.updUser(user2upd2));
+        final ValidationException e3 = assertThrows(ValidationException.class, () -> userController.updUser(user2upd3));
+
+        assertEquals("Не возможно обновить данные пользователя. Такого пользователя с таким id не существует.",
+                e0.getMessage());
+        assertEquals("updUser.user.email: должно иметь формат адреса электронной почты", e1.getMessage());
+        assertEquals("updUser.user.login: не должно быть пустым", e2.getMessage());
+        assertEquals("updUser.user.birthday: должно содержать прошедшую дату", e3.getMessage());
+    }
+
+    @Test
+    void checkGetUsers() {
+        var getAllBefore = userController.getUsers();
+        var sizeBefore = getAllBefore.size();
+        var user3 = new User();
+        user3.setEmail("user3@user.com");
+        user3.setLogin("login3");
+        user3.setName("username3");
+        user3.setBirthday(LocalDate.of(1989, 4, 23));
+        var addedUser3 = userController.addUser(user3);
+        var user4 = new User();
+        user4.setEmail("user4@user.com");
+        user4.setLogin("login4");
+        user4.setName("username4");
+        user4.setBirthday(LocalDate.of(1989, 4, 24));
+        var addedUser4 = userController.addUser(user4);
+
+        var getAllAfter = userController.getUsers();
+        var sizeAfter = getAllAfter.size();
+
+        assertNotNull(getAllAfter, "Метод вернул null");
+        assertFalse(getAllAfter.isEmpty(), "Список пользователей пуст");
+        assertEquals(sizeBefore + 2, sizeAfter, "Неверное количество пользователей.");
+        assertEquals(addedUser3, getAllAfter.get(sizeBefore), "Фильмы не совпадают.");
+        assertEquals(addedUser4, getAllAfter.get(sizeBefore + 1), "Фильмы не совпадают.");
     }
 }
